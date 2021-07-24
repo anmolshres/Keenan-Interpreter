3205132c1bbb5a84ff8a51e88d6c9788c59907f0
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2021-07-24 06:07:34 +0000

    add better checks and logs

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from textblob import TextBlob, exceptions
 from web_server import serve
 
 client = discord.Client()
-keenan = ["Tovarish#7985"]
+keenan = ["Tovarish#7985","carnage_roger#8850"]
 ERROR_CODE = 'ERROR'
 
 def translate(text,to_lang)->str:
@@ -34,17 +34,19 @@ async def on_message(message):
     except exceptions.TranslatorError:
       detected_language = ERROR_CODE      
     print(f'[LOG] Detected language: {detected_language}')
-    if detected_language != "en" or detected_language == ERROR_CODE and str(message.author) in keenan:
+    if message.content.lower().startswith('!keen'):
       if detected_language != ERROR_CODE:
-        translated_text = translate(text=message_text,to_lang='en')
-        await message.channel.send(f'The detected language is {detected_language} from {message.author} and its English translation is ```{translated_text}```')
+        message_text = message.content[5:]
+        translated_text = translate(text=message_text,to_lang='ru')
+        await message.channel.send(f'```{translated_text}```')
+        print(f'[LOG] Text that was translated: {message_text}')
       else:
         await message.channel.send(f'```Sorry, but text to be translated needs to be at least 3 characters long.```')
-    elif message.content.lower().startswith('!keen'):
+    elif (detected_language != "en" or detected_language == ERROR_CODE) and str(message.author) in keenan:
       if detected_language != ERROR_CODE:
-        message_text = message.content.split("!keen ",1)[1]
-        translated_text = translate(text=message_text,to_lang='ru')
-        await message.channel.send(f'The detected language is {detected_language} from {message.author} and its Russian translation is ```{translated_text}```')
+        translated_text = translate(text=message_text,to_lang='en')
+        await message.channel.send(f'```{translated_text}```')
+        print(f'[LOG] Text that was translated: {message_text}')
       else:
         await message.channel.send(f'```Sorry, but text to be translated needs to be at least 3 characters long.```')
       
