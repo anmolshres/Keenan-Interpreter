ca9360c1f324c434f8ba24a65d3bd68227ccf300
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2021-07-24 06:38:27 +0000

    updating logs more often

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from textblob import TextBlob, exceptions
 from web_server import serve
 
 client = discord.Client()
-keenan = ["Tovarish#7985","carnage_roger#8850"]
+keenan = ["Tovarish#7985","carnage_roger#8850","carnage_roger","Tovarish","Comrade General Товарищ"]
 ERROR_CODE = 'ERROR'
 
 def translate(text,to_lang)->str:
@@ -29,14 +29,22 @@ async def on_message(message):
         return
 
     message_text = message.content
+    print('[BLOCK]')
+    print(f'[LOG] Full text recieved: {message_text}')    
+    
+    for user in keenan:
+      if f'@{user} ' in message_text:
+        message_text = message_text.replace(f'@{user} ','')
+      elif f'@{user}' in message_text:
+        message_text = message_text.replace(f'@{user}','')
     try:
       detected_language = detect_lang(message_text)
     except exceptions.TranslatorError:
       detected_language = ERROR_CODE      
     print(f'[LOG] Detected language: {detected_language}')
-    if message.content.lower().startswith('!keen'):
+    if message_text.lower().startswith('!keen'):
       if detected_language != ERROR_CODE:
-        message_text = message.content[5:]
+        message_text = message_text[5:]
         translated_text = translate(text=message_text,to_lang='ru')
         await message.channel.send(f'```{translated_text}```')
         print(f'[LOG] Text that was translated: {message_text}')
