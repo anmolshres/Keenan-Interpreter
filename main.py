e8a4527433c8e4ed159f54da13512be291ec5639
Anmol Shrestha <shrestha.anmol.as@gmail.com>
2021-07-24 05:29:47 +0000

    translator error handling

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import discord
 import os
-from textblob import TextBlob
+from textblob import TextBlob, exceptions
 from web_server import serve
 
 client = discord.Client()
-keenan = ["Tovarish#7985","carnage_roger#8850"]
+keenan = ["Tovarish#7985"]
+ERROR_CODE = 'ERROR'
 
 def translate(text,to_lang)->str:
   text_blob = TextBlob(text)
@@ -25,15 +26,24 @@ async def on_message(message):
         return
 
     message_text = message.content
-    detected_language = detect_lang(message_text)
+    try:
+      detected_language = detect_lang(message_text)
+    except exceptions.TranslatorError:
+      detected_language = ERROR_CODE      
     print(f'[LOG] Detected language: {detected_language}')
     if detected_language != "en" and str(message.author) in keenan:
-      translated_text = translate(text=message_text,to_lang='en')
-      await message.channel.send(f'The detected language is {detected_language} from {message.author} and its English translation is ```{translated_text}```')
-    elif detected_language == "en" and message.content.lower().startswith('!keenan'):
-      message_text = message.content.split("!keen ",1)[1]
-      translated_text = translate(text=message_text,to_lang='ru')
-      await message.channel.send(f'The detected language is {detected_language} from {message.author} and its Russian translation is ```{translated_text}```')
+      if detected_language != ERROR_CODE:
+        translated_text = translate(text=message_text,to_lang='en')
+        await message.channel.send(f'The detected language is {detected_language} from {message.author} and its English translation is ```{translated_text}```')
+      else:
+        await message.channel.send(f'```Sorry, but text to be translated needs to be at least 3 characters long.```')
+    elif message.content.lower().startswith('!keen'):
+      if detected_language != ERROR_CODE:
+        message_text = message.content.split("!keen ",1)[1]
+        translated_text = translate(text=message_text,to_lang='ru')
+        await message.channel.send(f'The detected language is {detected_language} from {message.author} and its Russian translation is ```{translated_text}```')
+      else:
+        await message.channel.send(f'```Sorry, but text to be translated needs to be at least 3 characters long.```')
       
 serve()
 client.run(os.getenv('TOKEN'))
